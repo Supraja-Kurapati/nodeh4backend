7864c46ae039e5af2013c51e9ed6ffd45c525517
Supraja-Kurapati <kurapatisupraja8@gmail.com>
2024-01-22 19:25:16 +0530

    new

--- Routes.js
+++ Routes.js
@@ -1,6 +1,6 @@
 const express=require('express')
 const route= express.Router()
-const bcrypt=require('bcrypt')
+const bcrypt=require('bcryptjs')
 const jwt=require('jsonwebtoken')
 const secretkey="7ken"
 const saltRound=10;
